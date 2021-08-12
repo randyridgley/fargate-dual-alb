14f3bf56ef0fba2cda5087d8f2136fd5798cb815
Pahud <pahudnet@gmail.com>
2021-08-12 17:54:34 +0800

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,9 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'fargate-dual-alb',
+  
+
+});
+project.synth();
