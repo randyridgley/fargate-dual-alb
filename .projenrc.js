2762bd8df151cf14466fe035d64a6c536c54bcee
Pahud <pahudnet@gmail.com>
2021-08-13 11:46:31 +0800

    add codebuild-ci

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,14 @@
-const { AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp, DependenciesUpgradeMechanism } = require('projen');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.95.2',
   defaultReleaseBranch: 'main',
   name: 'fargate-dual-alb',
   description: 'Demo application with cdk-fargate-patterns',
   cdkDependencies: [
+    '@aws-cdk/aws-codebuild',
     '@aws-cdk/aws-ec2',
     '@aws-cdk/aws-ecs',
     '@aws-cdk/aws-iam',
@@ -17,7 +21,17 @@ const project = new AwsCdkTypeScriptApp({
   context: {
     '@aws-cdk/core:newStyleStackSynthesis': 'true',
   },
-
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['pahud'],
+  },
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'dependabot.yml'];
